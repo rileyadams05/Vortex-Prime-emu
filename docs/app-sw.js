6860628198013662839225d0cf663644ae14394e
Budm4n <rileyadams848@gmail.com>
2026-05-19 02:19:46 +1000

    Add installable web app support

---
+++ docs/app-sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "vortex-prime-app-v1";
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/site.webmanifest",
+  "/favicon.svg",
+  "/favicon.png",
+  "/pwa-icon-192.png",
+  "/pwa-icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("/")))
+  );
+});
